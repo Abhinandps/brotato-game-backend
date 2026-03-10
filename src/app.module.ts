ecc32fa84ff83c056f68ed6b636f2d725ecdd79a
Abhinand PS <psabhinand333s@gmail.com>
2026-03-10 19:10:24 +0530

    core module created

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { GameModule } from './game/game.module';
+import { RoomModule } from './room/room.module';
+import { PlayerModule } from './player/player.module';
+import { GatewayModule } from './gateway/gateway.module';
 
 @Module({
-  imports: [],
+  imports: [GameModule, RoomModule, PlayerModule, GatewayModule],
   controllers: [AppController],
   providers: [AppService],
 })
